c8483dc4e468b661011e8878c54b7045be6c2f50
Abraham S <abrahamsalloum@gmail.com>
2021-08-14 16:52:27 +1000

    style changes

--- src/features/FrontPage/storypage.js
+++ src/features/FrontPage/storypage.js
@@ -37,14 +37,13 @@ const StoryPage = ({id}) => {
     while ((!!details === false)) return <div>loading....</div>
     return(
         <div style={{ "display": "flex", "flexDirection": "column", "alignItems": "center" }}>
-            <div className={!!storyid ? "singlestory" : null} style={{"width":"100%"}}>
+            <div className={!!storyid ? "singlestory" : "ss"}>
             {!!storyid ? <div style={{padding: 5}}><Link to={"/"} target="_blank" rel="noopener noreferrer"><button>Home</button></Link></div>: null}
-
             <div className="bottominfo" style={{ "backgroundColor": stringToColour(details.id) }}>
                 <div className="infotextcontainer">
                     <div className="infotext">
                         <div>ID:</div>
-                        <div> <Link to={`/story/${details.id}`}  target="_blank" rel="noopener noreferrer">{details.id}</Link></div>
+                        <div><Link to={`/story/${details.id}`}  target="_blank" rel="noopener noreferrer">{details.id}</Link></div>
                     </div>
                     <div className="infotext">
                         <div>Type:</div>
@@ -55,15 +54,13 @@ const StoryPage = ({id}) => {
                         <div>{details.score}</div>
                     </div>
                 </div>
-
             </div>
             <div className="story_content_wrapper">
                 <div aria-label="storylink"><h3>{details.title} <a target="_blank" rel="noreferrer" href={`https://news.ycombinator.com/item?id=${id}`}>{id}</a></h3></div>
                 <p><a target="_blank"  rel="noreferrer" href={details.url}>{details.url}</a></p>
-                    <div className="story_content" dangerouslySetInnerHTML={createMarkup(details.text)} />
-                </div>
-
-                <SubComments comments={details.kids} />
+                <div className="story_content" dangerouslySetInnerHTML={createMarkup(details.text)} />
+            </div>
+            <SubComments comments={details.kids} />
             </div>
         </div>
     )
